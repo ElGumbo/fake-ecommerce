46f6da4df3ea7941f51b736f36e0486410d2dc6c
Gumba <jeromontz@hotmail.de>
2026-05-11 11:30:44 +0200

    add fake store api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import ProductGrid from "./components/ProductGrid";
+import Header from "./components/shared/Header";
 
 function App() {
+  const [storeData, setStoreData] = useState([]);
+
+  useEffect(() => {
+    const fetchStoreData = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setStoreData(data);
+    };
+    fetchStoreData();
+  }, []);
+
+  console.log(storeData);
 
   return (
     <>
-      
+      <Header />
+      <ProductGrid products={storeData} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
