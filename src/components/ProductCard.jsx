8a92a7e7816ecda522ca824ec1312b8cdcb5ad6e
Gumba <jeromontz@hotmail.de>
2026-05-11 13:15:16 +0200

    add add to cart and increase/decrease quantity buttons

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ProductCard({ product }) {
   const price = new Intl.NumberFormat("de-DE", {
@@ -6,9 +6,44 @@ export default function ProductCard({ product }) {
     currency: "EUR",
   }).format(product.price);
 
+  const getQuantity = () => {
+    const cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
+    return cartItems.find((item) => item.id === product.id)?.quantity ?? 0;
+  };
+
+  const [quantity, setQuantity] = useState(getQuantity);
+
+  const addToCart = (product) => {
+    const cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
+    cartItems.push({ ...product, quantity: 1 });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    setQuantity(getQuantity());
+  };
+
+  const incrementQuantity = () => {
+    const cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
+    const currentItem = cartItems.find((item) => item.id === product.id);
+    currentItem.quantity += 1;
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    setQuantity(getQuantity());
+  };
+
+  const decrementQuantity = () => {
+    const cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
+    const currentItem = cartItems.find((item) => item.id === product.id);
+    currentItem.quantity -= 1;
+    if (currentItem.quantity <= 0) {
+      const filtered = cartItems.filter((item) => item.id !== product.id);
+      localStorage.setItem("cartItems", JSON.stringify(filtered));
+    } else {
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    }
+    setQuantity(getQuantity());
+  };
+
   return (
     <div className='card bg-base-100 w-full h-full shadow-sm'>
-      <figure className="h-64">
+      <figure className='h-64'>
         <img
           src={product.image}
           alt={product.title}
@@ -19,7 +54,24 @@ export default function ProductCard({ product }) {
         <h2 className='card-title'>{product.title}</h2>
         <p>{price}</p>
         <div className='card-actions justify-end'>
-          <button className='btn btn-primary'>Buy Now</button>
+          {quantity === 0 ? (
+            <button
+              onClick={() => addToCart(product)}
+              className='btn btn-primary'
+            >
+              Add to cart
+            </button>
+          ) : (
+            <div className='flex items-center gap-2'>
+              <button onClick={decrementQuantity} className='btn btn-primary'>
+                -
+              </button>
+              <span>Quantity: {quantity}</span>
+              <button onClick={incrementQuantity} className='btn btn-primary'>
+                +
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
